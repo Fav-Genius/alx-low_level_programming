ab0dacf12f77037e38266c4ff095b2f62e68843d
Favour <75821328+Fav-Genius@users.noreply.github.com>
2022-09-12 21:24:46 +0100

    Update 3-print_alphabets.c

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -6,17 +6,18 @@
  * this program prints "Programming is positive, zero, or negative
  * Return: 0
  */
+
 int main(void)
 {
 char ch;
-for (ch = a ; ch <= z ; ch++)
+for (ch = 'a' ; ch <= 'z' ; ch++)
 {
-putchar(ch);
+	putchar(ch);
 }
-for (ch = A ; ch <= Z ; ch++)
+for (ch = 'A' ; ch <= 'Z' ; ch++)
 {
-putchar(ch);
+	putchar(ch);
 }
-putchar(n);
+putchar('\n');
 return (0);
 }
