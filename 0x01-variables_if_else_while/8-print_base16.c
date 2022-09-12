7ed2a50bc300df095b961688a068a411ab383abf
Favour <75821328+Fav-Genius@users.noreply.github.com>
2022-09-12 22:39:00 +0100

    Update 8-print_base16.c

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -9,13 +9,13 @@
 int main(void)
 {
 char c;
-for (c = 0; c <= 9; c++)
+for (c = '0'; c <= '9'; c++)
 {
-putchar(c);
+	putchar(c);
 }
-for (c = a; c <= f; c++)
+for (c = 'a'; c <= 'f'; c++)
 {
-putchar(c);
+	putchar(c);
 }
 putchar('\n');
 return (0);
