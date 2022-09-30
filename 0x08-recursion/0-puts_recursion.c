ee84ca56b41d63b1d645aae8ff42f1c0e4fd4b6a
Fav-Genius <favourj35@gmail.com>
2022-09-29 17:02:11 -0700

    task 0 executable file

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * _puts_recursion - Prints a string, followed by a new line.
+ * @s: The string to be printed.
+ */
+void _puts_recursion(char *s)
+{
+0-puts_recursion.c if (*s)
+0-puts_recursion.c {
+0-puts_recursion.c 0-puts_recursion.c _putchar(*s);
+0-puts_recursion.c 0-puts_recursion.c _puts_recursion(s + 1);
+0-puts_recursion.c }
+
+0-puts_recursion.c else
+0-puts_recursion.c 0-puts_recursion.c _putchar(n);
+}
---
+++ 0x08-recursion/README.md
@@ -0,0 +1,1 @@
+First readme on Recursion in c
